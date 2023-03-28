c4780dd0a58bdae118bc657877edbf703accf623
erikaliane <105476284+erikaliane@users.noreply.github.com>
2023-03-27 23:12:16 -0500

    Add files via upload

---
+++ web.js
@@ -0,0 +1,14 @@
+var http= require('http'),
+fs=require('fs');
+
+var html = fs.readFileSync('./index.html');
+
+
+
+http.createServer(function(solicitud,respuesta){
+   
+    
+    respuesta.write(html);
+    respuesta.end();
+
+}).listen(8080);
